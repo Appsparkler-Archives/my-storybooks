cf4ed515bd3374e9c82b37338814e5aa66e5d4c6
Aakash Shah <subs@appsparkler.com>
2021-03-04 16:42:46 +0530

    setting up compiling for react-mark.js

---
+++ packages/react-mark.js/dist/index.js
@@ -0,0 +1,31 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "RegExpMarker", {
+  enumerable: true,
+  get: function get() {
+    return _RegExpMarker["default"];
+  }
+});
+Object.defineProperty(exports, "Marker", {
+  enumerable: true,
+  get: function get() {
+    return _Marker["default"];
+  }
+});
+Object.defineProperty(exports, "RangesMarker", {
+  enumerable: true,
+  get: function get() {
+    return _RangesMarker["default"];
+  }
+});
+
+var _RegExpMarker = _interopRequireDefault(require("./components/RegExpMarker"));
+
+var _Marker = _interopRequireDefault(require("./components/Marker"));
+
+var _RangesMarker = _interopRequireDefault(require("./components/RangesMarker"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
