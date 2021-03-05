3bf1b29c841cc85d63031c0c6064526876e72272
Aakash Shah <subs@appsparkler.com>
2021-03-06 05:21:51 +0530

    Setting up clock package

---
+++ .storybook/clock/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "stories": [
+    // "../stories/**/*.stories.mdx",
+    // "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../../packages/clock/src/**/*.stories.js"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
