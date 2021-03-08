b27b13d2937c88794cce0d30fee188367a6da36d
Aakash Shah <subs@appsparkler.com>
2021-03-08 11:56:44 +0530

    Renamed SB config path

---
+++ .storybook/react-clocks/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  stories: [
+    // "../stories/**/*.stories.mdx",
+    // "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../../packages/react-clocks/src/**/*.stories.js",
+  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+};
