a6d482e226328a40a4d8521bc2abc285a59fc27c
Aakash Shah <subs@appsparkler.com>
2021-03-11 12:03:13 +0530

    Dynamic heights for each time-unit

--- packages/react-clocks/src/components/WaveClock/TimeDisplay/index.js
+++ packages/react-clocks/src/components/WaveClock/TimeDisplay/index.js
@@ -1,8 +1,18 @@
 import { useMemo, memo } from "react";
 
 const DisplayedTime = ({ hours, minutes, seconds }) => {
-  const { displayHours, displayMinutes, displaySeconds } = useMemo(
+  const {
+    hoursWaveHeightPercent,
+    displayHours,
+    displayMinutes,
+    displaySeconds,
+    minutesWaveHeightPercent,
+    secondsWaveHeightPercent,
+  } = useMemo(
     () => ({
+      secondsWaveHeightPercent: `${(seconds / 60) * 100}%`,
+      minutesWaveHeightPercent: `${(minutes / 60) * 100}%`,
+      hoursWaveHeightPercent: `${(hours / 24) * 100}%`,
       displayHours: hours < 10 ? `0${hours}` : hours,
       displayMinutes: minutes < 10 ? `0${minutes}` : minutes,
       displaySeconds: seconds < 10 ? `0${seconds}` : seconds,
@@ -10,10 +20,30 @@ const DisplayedTime = ({ hours, minutes, seconds }) => {
     [hours, minutes, seconds]
   );
   return (
-    <div className="text-overlay">
-      <div>{hours}</div>
-      <div>{minutes}</div>
-      <div>{seconds}</div>
+    <div className="App">
+      <div className="Column">
+        <div
+          className="Marker"
+          style={{ height: hoursWaveHeightPercent, opacity: "abcd" }}
+        ></div>
+      </div>
+      <div className="Column">
+        <div
+          className="Marker"
+          style={{ height: minutesWaveHeightPercent, opacity: "abcd" }}
+        ></div>
+      </div>
+      <div className="Column">
+        <div
+          className="Marker Marker--seconds"
+          style={{ height: secondsWaveHeightPercent, opacity: "abcd" }}
+        ></div>
+      </div>
+      <div className="text-overlay">
+        <div>{displayHours}</div>
+        <div>{displayMinutes}</div>
+        <div>{displaySeconds}</div>
+      </div>
     </div>
   );
 };
