85244194d1a187e01b2ab3ccb58850e2ef122b2a
Aakash Shah <subs@appsparkler.com>
2021-03-23 13:14:26 +0530

    Setting up React Bootstrap 5

---
+++ .storybook/react-bootstrap-5/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  stories: [
+    // "../stories/**/*.stories.mdx",
+    // "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../../packages/react-bootstrap-5/src/**/*.stories.js",
+  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+};
---
+++ packages/react-bootstrap-5/src/components/Badge.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Template = (args) => <div>Badge</div>;
+
+export const Default = Template.bind({});
+Default.args = {};
+
+const Story = {
+  title: "Components/Badge",
+};
+
+export default Story;
