03bd358021e774a20ee3e54fc7dc928aec3a5395
Aakash Shah <subs@appsparkler.com>
2021-03-24 05:51:38 +0530

    working accordian

--- .storybook/react-bootstrap-5/preview.js
+++ .storybook/react-bootstrap-5/preview.js
@@ -1,4 +1,5 @@
 import Carousel from "../../packages/react-bootstrap-5/node_modules/bootstrap/js/dist/carousel";
+import Accordian from "../../packages/react-bootstrap-5/node_modules/bootstrap/js/dist/collapse";
 
 console.log({ Carousel });
 export const parameters = {
---
+++ packages/react-bootstrap-5/src/components/Accordian/index.stories.js
@@ -0,0 +1,13 @@
+import Accordian from "./index";
+
+const Template = () => <Accordian />;
+
+export const Default = Template.bind({});
+Default.args = {};
+
+const Story = {
+  title: "Components/Accordian",
+  component: Accordian,
+};
+
+export default Story;
