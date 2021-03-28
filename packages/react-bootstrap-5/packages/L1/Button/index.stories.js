2668f755c2f6dac2a8e55ecc40b778198a597566
Aakash Shah <subs@appsparkler.com>
2021-03-28 20:45:51 +0530

    Updated stories

--- packages/react-bootstrap-5/packages/L1/Button/index.stories.js
+++ packages/react-bootstrap-5/packages/L1/Button/index.stories.js
@@ -66,93 +66,118 @@ export const Small = () => {
 
 export const Medium = () => {
   return (
-    <div className="d-flex flex-row gap-2 flex-wrap">
-      <Button primary md>
-        Primary
-      </Button>
-      <Button secondary md>
-        Secondary
-      </Button>
-      <Button success md>
-        Success
-      </Button>
-      <Button danger md>
-        Danger
-      </Button>
-      <Button warning md>
-        Warning
-      </Button>
-      <Button info md>
-        Info
-      </Button>
-      <Button light md>
-        Light
-      </Button>
-      <Button dark md>
-        Dark
-      </Button>
+    <div className="d-flex flex-column gap-2 flex-wrap">
+      <div className="d-flex flex-row gap-2 flex-wrap">
+        <Button primary md>
+          Primary
+        </Button>
+        <Button secondary md>
+          Secondary
+        </Button>
+        <Button success md>
+          Success
+        </Button>
+        <Button danger md>
+          Danger
+        </Button>
+        <Button warning md>
+          Warning
+        </Button>
+        <Button info md>
+          Info
+        </Button>
+        <Button light md>
+          Light
+        </Button>
+        <Button dark md>
+          Dark
+        </Button>
+      </div>
+      <div className="d-flex flex-row gap-2 flex-wrap">
+        <Button primary md disabled>
+          Primary
+        </Button>
+        <Button secondary md disabled>
+          Secondary
+        </Button>
+        <Button success md disabled>
+          Success
+        </Button>
+        <Button danger md disabled>
+          Danger
+        </Button>
+        <Button warning md disabled>
+          Warning
+        </Button>
+        <Button info md disabled>
+          Info
+        </Button>
+        <Button light md disabled>
+          Light
+        </Button>
+        <Button dark md disabled>
+          Dark
+        </Button>
+      </div>
     </div>
   );
 };
 
 export const Large = () => {
   return (
-    <div className="d-flex flex-row gap-2 flex-wrap">
-      <Button primary lg>
-        Primary
-      </Button>
-      <Button secondary lg>
-        Secondary
-      </Button>
-      <Button success lg>
-        Success
-      </Button>
-      <Button danger lg>
-        Danger
-      </Button>
-      <Button warning lg>
-        Warning
-      </Button>
-      <Button info lg>
-        Info
-      </Button>
-      <Button light lg>
-        Light
-      </Button>
-      <Button dark lg>
-        Dark
-      </Button>
-    </div>
-  );
-};
-
-export const Disabled = () => {
-  return (
-    <div className="d-flex flex-row gap-2 flex-wrap">
-      <Button primary sm outline disabled>
-        Primary
-      </Button>
-      <Button secondary sm outline disabled>
-        Secondary
-      </Button>
-      <Button success sm outline disabled>
-        Success
-      </Button>
-      <Button danger sm outline disabled>
-        Danger
-      </Button>
-      <Button warning sm outline disabled>
-        Warning
-      </Button>
-      <Button info sm outline disabled>
-        Info
-      </Button>
-      <Button light sm outline disabled>
-        Light
-      </Button>
-      <Button dark sm outline disabled>
-        Dark
-      </Button>
+    <div className="d-flex flex-column gap-2 flex-wrap">
+      <div className="d-flex flex-row gap-2 flex-wrap">
+        <Button primary lg>
+          Primary
+        </Button>
+        <Button secondary lg>
+          Secondary
+        </Button>
+        <Button success lg>
+          Success
+        </Button>
+        <Button danger lg>
+          Danger
+        </Button>
+        <Button warning lg>
+          Warning
+        </Button>
+        <Button info lg>
+          Info
+        </Button>
+        <Button light lg>
+          Light
+        </Button>
+        <Button dark lg>
+          Dark
+        </Button>
+      </div>
+      <div className="d-flex flex-row gap-2 flex-wrap">
+        <Button primary lg disabled>
+          Primary
+        </Button>
+        <Button secondary lg disabled>
+          Secondary
+        </Button>
+        <Button success lg disabled>
+          Success
+        </Button>
+        <Button danger lg disabled>
+          Danger
+        </Button>
+        <Button warning lg disabled>
+          Warning
+        </Button>
+        <Button info lg disabled>
+          Info
+        </Button>
+        <Button light lg disabled>
+          Light
+        </Button>
+        <Button dark lg disabled>
+          Dark
+        </Button>
+      </div>
     </div>
   );
 };
