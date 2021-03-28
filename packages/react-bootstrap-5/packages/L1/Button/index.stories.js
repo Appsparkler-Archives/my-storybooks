f2774265ccb149878e56083aa296fba8b334157c
Aakash Shah <subs@appsparkler.com>
2021-03-28 17:42:29 +0530

    Added button stories

--- packages/react-bootstrap-5/packages/L1/Button/index.stories.js
+++ packages/react-bootstrap-5/packages/L1/Button/index.stories.js
@@ -102,6 +102,68 @@ export const Small = () => {
   );
 };
 
+export const Medium = () => {
+  return (
+    <div className="d-flex flex-row gap-2 flex-wrap">
+      <Button primary md>
+        Primary
+      </Button>
+      <Button secondary md>
+        Secondary
+      </Button>
+      <Button success md>
+        Success
+      </Button>
+      <Button danger md>
+        Danger
+      </Button>
+      <Button warning md>
+        Warning
+      </Button>
+      <Button info md>
+        Info
+      </Button>
+      <Button light md>
+        Light
+      </Button>
+      <Button dark md>
+        Dark
+      </Button>
+    </div>
+  );
+};
+
+export const Large = () => {
+  return (
+    <div className="d-flex flex-row gap-2 flex-wrap">
+      <Button primary lg>
+        Primary
+      </Button>
+      <Button secondary lg>
+        Secondary
+      </Button>
+      <Button success lg>
+        Success
+      </Button>
+      <Button danger lg>
+        Danger
+      </Button>
+      <Button warning lg>
+        Warning
+      </Button>
+      <Button info lg>
+        Info
+      </Button>
+      <Button light lg>
+        Light
+      </Button>
+      <Button dark lg>
+        Dark
+      </Button>
+    </div>
+  );
+};
+
 const Story = {
   title: "L1/Buttons",
 };
