8c59ea7a2b7080be6f92469b0df7f3c94095ccd8
Aakash Shah <subs@appsparkler.com>
2021-03-28 17:16:42 +0530

    Movnig variant extractor to hook

--- packages/react-bootstrap-5/packages/L1/Button/index.stories.js
+++ packages/react-bootstrap-5/packages/L1/Button/index.stories.js
@@ -1,5 +1,53 @@
 import React from "react";
 
+const useVariant = (props = {}) => {
+  const {
+    primary,
+    secondary,
+    info,
+    success,
+    warning,
+    danger,
+    light,
+    dark,
+    muted,
+    ...restProps
+  } = props;
+
+  const variant = React.useMemo(() => {
+    if (primary) return "primary";
+    if (secondary) return "secondary";
+    if (info) return "info";
+    if (success) return "success";
+    if (warning) return "warning";
+    if (danger) return "danger";
+    if (light) return "light";
+    if (dark) return "dark";
+    if (muted) return "muted";
+  }, [primary, secondary, info, success, warning, danger, light, dark, muted]);
+
+  return {
+    variant,
+    restProps,
+  };
+};
+
+const PlainButton = (props, ref) => {
+  const { variant, restProps } = useVariant(props);
+  const { children, className, ...restProps2 } = restProps;
+  return (
+    <button
+      ref={ref}
+      className={`btn btn-${variant} ${className}`}
+      {...restProps2}
+    >
+      {children}
+    </button>
+  );
+};
+
+const Button = React.forwardRef(PlainButton);
+
 export const All = () => {
   return (
     <table className="table">
@@ -13,7 +61,9 @@ export const All = () => {
       <tbody>
         <tr>
           <td>
-            <button className="btn btn-primary btn-sm">Primary</button>
+            <Button primary className="btn-sm">
+              Primary
+            </Button>
           </td>
           <td>
             <button className="btn btn-primary btn-md">Primary</button>
