e6743b5a97aa61ddc81bc2734dff0bab174ceabb
Aakash Shah <subs@appsparkler.com>
2021-03-28 13:04:06 +0530

    Setting up packages inside react-bootstrap-5 package

--- .storybook/react-bootstrap-5/main.js
+++ .storybook/react-bootstrap-5/main.js
@@ -3,7 +3,7 @@ module.exports = {
     // "../stories/**/*.stories.mdx",
     // "../stories/**/*.stories.@(js|jsx|ts|tsx)"
     "../../packages/react-bootstrap-5/src/**/*.stories.js",
-    "../../packages/react-bootstrap-5/L1/**/*.stories.js",
+    "../../packages/react-bootstrap-5/packages/**/*.stories.js",
   ],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
 };
