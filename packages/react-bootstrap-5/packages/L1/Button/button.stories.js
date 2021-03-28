a1668ef4a64e4212d81cb50b4eebcf6c25d6ad27
Aakash Shah <subs@appsparkler.com>
2021-03-28 20:49:24 +0530

    updating stories

--- packages/react-bootstrap-5/packages/L1/Button/button.stories.js
+++ packages/react-bootstrap-5/packages/L1/Button/button.stories.js
@@ -7,124 +7,177 @@ Button.defaultProps = {
 
 export const Small = () => {
   return (
-    <div className="d-flex flex-row gap-2 flex-wrap">
-      <Button primary sm outline>
-        Primary
-      </Button>
-      <Button secondary sm outline>
-        Secondary
-      </Button>
-      <Button success sm outline>
-        Success
-      </Button>
-      <Button danger sm outline>
-        Danger
-      </Button>
-      <Button warning sm outline>
-        Warning
-      </Button>
-      <Button info sm outline>
-        Info
-      </Button>
-      <Button light sm outline>
-        Light
-      </Button>
-      <Button dark sm outline>
-        Dark
-      </Button>
+    <div className="d-flex flex-column gap-2 flex-wrap">
+      <div className="d-flex flex-row gap-2 flex-wrap">
+        <Button primary sm outline>
+          Primary
+        </Button>
+        <Button secondary sm outline>
+          Secondary
+        </Button>
+        <Button success sm outline>
+          Success
+        </Button>
+        <Button danger sm outline>
+          Danger
+        </Button>
+        <Button warning sm outline>
+          Warning
+        </Button>
+        <Button info sm outline>
+          Info
+        </Button>
+        <Button light sm outline>
+          Light
+        </Button>
+        <Button dark sm outline>
+          Dark
+        </Button>
+      </div>
+      <div className="d-flex flex-row gap-2 flex-wrap">
+        <Button primary sm disabled outline>
+          Primary
+        </Button>
+        <Button secondary sm disabled outline>
+          Secondary
+        </Button>
+        <Button success sm disabled outline>
+          Success
+        </Button>
+        <Button danger sm disabled outline>
+          Danger
+        </Button>
+        <Button warning sm disabled outline>
+          Warning
+        </Button>
+        <Button info sm disabled outline>
+          Info
+        </Button>
+        <Button light sm disabled outline>
+          Light
+        </Button>
+        <Button dark sm disabled outline>
+          Dark
+        </Button>
+      </div>
     </div>
   );
 };
 
 export const Medium = () => {
   return (
-    <div className="d-flex flex-row gap-2 flex-wrap">
-      <Button primary md outline>
-        Primary
-      </Button>
-      <Button secondary md outline>
-        Secondary
-      </Button>
-      <Button success md outline>
-        Success
-      </Button>
-      <Button danger md outline>
-        Danger
-      </Button>
-      <Button warning md outline>
-        Warning
-      </Button>
-      <Button info md outline>
-        Info
-      </Button>
-      <Button light md outline>
-        Light
-      </Button>
-      <Button dark md outline>
-        Dark
-      </Button>
+    <div className="d-flex flex-column gap-2 flex-wrap">
+      <div className="d-flex flex-row gap-2 flex-wrap">
+        <Button primary md outline>
+          Primary
+        </Button>
+        <Button secondary md outline>
+          Secondary
+        </Button>
+        <Button success md outline>
+          Success
+        </Button>
+        <Button danger md outline>
+          Danger
+        </Button>
+        <Button warning md outline>
+          Warning
+        </Button>
+        <Button info md outline>
+          Info
+        </Button>
+        <Button light md outline>
+          Light
+        </Button>
+        <Button dark md outline>
+          Dark
+        </Button>
+      </div>
+      <div className="d-flex flex-row gap-2 flex-wrap">
+        <Button primary md disabled outline>
+          Primary
+        </Button>
+        <Button secondary md disabled outline>
+          Secondary
+        </Button>
+        <Button success md disabled outline>
+          Success
+        </Button>
+        <Button danger md disabled outline>
+          Danger
+        </Button>
+        <Button warning md disabled outline>
+          Warning
+        </Button>
+        <Button info md disabled outline>
+          Info
+        </Button>
+        <Button light md disabled outline>
+          Light
+        </Button>
+        <Button dark md disabled outline>
+          Dark
+        </Button>
+      </div>
     </div>
   );
 };
 
 export const Large = () => {
   return (
-    <div className="d-flex flex-row gap-2 flex-wrap">
-      <Button primary lg outline>
-        Primary
-      </Button>
-      <Button secondary lg outline>
-        Secondary
-      </Button>
-      <Button success lg outline>
-        Success
-      </Button>
-      <Button danger lg outline>
-        Danger
-      </Button>
-      <Button warning lg outline>
-        Warning
-      </Button>
-      <Button info lg outline>
-        Info
-      </Button>
-      <Button light lg outline>
-        Light
-      </Button>
-      <Button dark lg outline>
-        Dark
-      </Button>
-    </div>
-  );
-};
-
-export const Disabled = () => {
-  return (
-    <div className="d-flex flex-row gap-2 flex-wrap">
-      <Button primary lg outline disabled>
-        Primary
-      </Button>
-      <Button secondary lg outline disabled>
-        Secondary
-      </Button>
-      <Button success lg outline disabled>
-        Success
-      </Button>
-      <Button danger lg outline disabled>
-        Danger
-      </Button>
-      <Button warning lg outline disabled>
-        Warning
-      </Button>
-      <Button info lg outline disabled>
-        Info
-      </Button>
-      <Button light lg outline disabled>
-        Light
-      </Button>
-      <Button dark lg outline disabled>
-        Dark
-      </Button>
+    <div className="d-flex flex-column gap-2 flex-wrap">
+      <div className="d-flex flex-row gap-2 flex-wrap">
+        <Button primary lg outline>
+          Primary
+        </Button>
+        <Button secondary lg outline>
+          Secondary
+        </Button>
+        <Button success lg outline>
+          Success
+        </Button>
+        <Button danger lg outline>
+          Danger
+        </Button>
+        <Button warning lg outline>
+          Warning
+        </Button>
+        <Button info lg outline>
+          Info
+        </Button>
+        <Button light lg outline>
+          Light
+        </Button>
+        <Button dark lg outline>
+          Dark
+        </Button>
+      </div>
+      <div className="d-flex flex-row gap-2 flex-wrap">
+        <Button primary lg disabled outline>
+          Primary
+        </Button>
+        <Button secondary lg disabled outline>
+          Secondary
+        </Button>
+        <Button success lg disabled outline>
+          Success
+        </Button>
+        <Button danger lg disabled outline>
+          Danger
+        </Button>
+        <Button warning lg disabled outline>
+          Warning
+        </Button>
+        <Button info lg disabled outline>
+          Info
+        </Button>
+        <Button light lg disabled outline>
+          Light
+        </Button>
+        <Button dark lg disabled outline>
+          Dark
+        </Button>
+      </div>
     </div>
   );
 };
