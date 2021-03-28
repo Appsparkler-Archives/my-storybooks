ab3e2fbbfa8012f761145bfddb30c762cd22095b
Aakash Shah <subs@appsparkler.com>
2021-03-28 21:37:54 +0530

    Updated all-components-stories

---
+++ packages/react-bootstrap-5/packages/L1/all.stories.js
@@ -0,0 +1,46 @@
+import React from "react";
+import * as ButtonStories from "./Button/index.stories";
+import * as ButtonOutlineStories from "./Button/button.stories";
+
+export const Everything = () => (
+  <div className="d-flex flex-column gap-4">
+    <div className="p-3 shadow shadow-md bg-light">
+      <div className="d-flex flex-column gap-3">
+        <h3>Buttons</h3>
+        <ButtonStories.Small />
+        <ButtonStories.Medium />
+        <ButtonStories.Large />
+      </div>
+    </div>
+    <div className="p-3 shadow shadow-md bg-dark">
+      <div className="d-flex flex-column gap-3">
+        <h3 className="text-light">Buttons</h3>
+        <ButtonStories.Small />
+        <ButtonStories.Medium />
+        <ButtonStories.Large />
+      </div>
+    </div>
+    <div className="p-3 shadow shadow-md bg-light">
+      <div className="d-flex flex-column gap-3">
+        <h3>Buttons With Outline</h3>
+        <ButtonOutlineStories.Small />
+        <ButtonOutlineStories.Medium />
+        <ButtonOutlineStories.Large />
+      </div>
+    </div>
+    <div className="p-3 shadow shadow-md bg-dark">
+      <div className="d-flex flex-column gap-3">
+        <h3 className="text-light">Buttons With Outline</h3>
+        <ButtonOutlineStories.Small />
+        <ButtonOutlineStories.Medium />
+        <ButtonOutlineStories.Large />
+      </div>
+    </div>
+  </div>
+);
+
+const Story = {
+  title: "L1/All Components",
+};
+
+export default Story;
