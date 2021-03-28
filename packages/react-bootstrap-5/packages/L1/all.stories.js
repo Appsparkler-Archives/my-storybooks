c6c6849e37c4d0fc597f54b968317497b0851069
Aakash Shah <subs@appsparkler.com>
2021-03-28 21:42:34 +0530

    included Badge stories

--- packages/react-bootstrap-5/packages/L1/all.stories.js
+++ packages/react-bootstrap-5/packages/L1/all.stories.js
@@ -1,7 +1,7 @@
 import React from "react";
 import * as ButtonStories from "./Button/index.stories";
 import * as ButtonOutlineStories from "./Button/button.stories";
-
+import * as BadgeStories from "./Badge/index.stories";
 export const Everything = () => (
   <div className="d-flex flex-column gap-4">
     <div className="p-3 shadow shadow-md bg-light">
@@ -34,6 +34,20 @@ export const Everything = () => (
         <ButtonOutlineStories.Small />
         <ButtonOutlineStories.Medium />
         <ButtonOutlineStories.Large />
+      </div>
+    </div>
+    <div className="p-3 shadow shadow-md bg-light">
+      <div className="d-flex flex-column gap-3">
+        <h3>Badges</h3>
+        <BadgeStories.Simple />
+        <BadgeStories.Pill />
+      </div>
+    </div>
+    <div className="p-3 shadow shadow-md bg-dark">
+      <div className="d-flex flex-column gap-3">
+        <h3 className="text-light">Badges</h3>
+        <BadgeStories.Simple />
+        <BadgeStories.Pill />
       </div>
     </div>
   </div>
