511d78402d2a7023a33e7cbfef5e3a25bd0a3098
Aakash Shah <subs@appsparkler.com>
2021-03-31 21:26:02 +0530

    chore: clean up abstracted out code

--- multi-packages/react-bootstrap-5/L1/Button/index.js
+++ multi-packages/react-bootstrap-5/L1/Button/index.js
@@ -4,52 +4,6 @@ import {
   useSizeExtractor,
 } from "@react-bootstrap-5/hooks";
 
-const useVariant = (props = {}) => {
-  const {
-    primary,
-    secondary,
-    info,
-    success,
-    warning,
-    danger,
-    light,
-    dark,
-    muted,
-    ...restProps
-  } = props;
-
-  const variant = React.useMemo(() => {
-    if (primary) return "primary";
-    if (secondary) return "secondary";
-    if (info) return "info";
-    if (success) return "success";
-    if (warning) return "warning";
-    if (danger) return "danger";
-    if (light) return "light";
-    if (dark) return "dark";
-    if (muted) return "muted";
-  }, [primary, secondary, info, success, warning, danger, light, dark, muted]);
-
-  return {
-    variant,
-    restProps,
-  };
-};
-
-const useSize = (props = {}) => {
-  const { sm, md, lg, ...restProps } = props;
-  const size = React.useMemo(() => {
-    if (sm) return "sm";
-    if (md) return "md";
-    if (lg) return "lg";
-  }, [sm, md, lg]);
-
-  return {
-    restProps,
-    size,
-  };
-};
-
 const Button = (props, ref) => {
   const { active, ...afterActiveProps } = props;
   const { variant, restProps: afterVariantProps } = useVariantExtractor(
@@ -69,7 +23,6 @@ const Button = (props, ref) => {
     if (size) return `btn-${size}`;
     return "";
   }, [size]);
-
   const activeClass = React.useMemo(() => {
     if (active) return "active";
     return "";
