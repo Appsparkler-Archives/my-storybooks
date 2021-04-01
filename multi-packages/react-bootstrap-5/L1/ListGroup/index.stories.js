e837fd90b78eae301041f0a0b50276dbe9d33538
Aakash Shah <subs@appsparkler.com>
2021-04-02 04:47:12 +0530

    feat: adding stories for List Group

--- multi-packages/react-bootstrap-5/L1/ListGroup/index.stories.js
+++ multi-packages/react-bootstrap-5/L1/ListGroup/index.stories.js
@@ -1,8 +1,14 @@
 import React from "react";
 
-const ListGroup = ({ children, as: As }) => (
-  <As class="list-group">{children}</As>
-);
+const ListGroup = ({ children, className, flush, as: As }) => {
+  const flushClass = React.useMemo(() => {
+    if (flush) return `list-group-flush`;
+    return "";
+  }, []);
+  return (
+    <As className={`list-group ${flushClass} ${className}`}>{children}</As>
+  );
+};
 
 ListGroup.defaultProps = {
   as: "ul",
@@ -13,12 +19,18 @@ const ListGroupItem = (props) => {
   const activeClass = React.useMemo(() => {
     return active ? `active` : "";
   }, [active]);
+  const actionClass = React.useMemo(() => {
+    const isLink = As === "a";
+    const isButton = As === "button";
+    if (isLink || isButton) return "list-group-item-action";
+    return "";
+  }, [As]);
   const disabledClass = React.useMemo(() => {
     return disabled ? "disabled" : "";
   }, [disabled]);
   return (
     <As
-      class={`list-group-item ${activeClass} ${disabledClass} `}
+      className={`list-group-item ${activeClass} ${actionClass} ${disabledClass} `}
       {...restProps}
     >
       {children}
@@ -58,6 +70,56 @@ export const WithDisabledItem = () => (
     <ListGroupItem>An third item</ListGroupItem>
     <ListGroupItem>A fourth item</ListGroupItem>
     <ListGroupItem>And a fifth one</ListGroupItem>
+  </ListGroup>
+);
+
+export const Links = () => (
+  <ListGroup>
+    <ListGroupItem active as="a" href="#">
+      The current link item
+    </ListGroupItem>
+    <ListGroupItem as="a" href="#">
+      A second link item
+    </ListGroupItem>
+    <ListGroupItem as="a" href="#">
+      A third link item
+    </ListGroupItem>
+    <ListGroupItem as="a" href="#">
+      A fourth link item
+    </ListGroupItem>
+    <ListGroupItem as="a" href="#">
+      A disabled link item
+    </ListGroupItem>
+  </ListGroup>
+);
+
+export const Buttons = () => (
+  <ListGroup>
+    <ListGroupItem active as="button" type="button">
+      The current link item
+    </ListGroupItem>
+    <ListGroupItem as="button" type="button">
+      A second link item
+    </ListGroupItem>
+    <ListGroupItem as="button" type="button">
+      A third link item
+    </ListGroupItem>
+    <ListGroupItem as="button" type="button">
+      A fourth link item
+    </ListGroupItem>
+    <ListGroupItem as="button" type="button">
+      A disabled link item
+    </ListGroupItem>
+  </ListGroup>
+);
+
+export const Flush = () => (
+  <ListGroup flush>
+    <li className="list-group-item">An item</li>
+    <li className="list-group-item">A second item</li>
+    <li className="list-group-item">A third item</li>
+    <li className="list-group-item">A fourth item</li>
+    <li className="list-group-item">And a fifth one</li>
   </ListGroup>
 );
 
