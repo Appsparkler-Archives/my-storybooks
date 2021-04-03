8bcb07f43e4cad55c9794cf9502945fbb8407394
Aakash Shah <subs@appsparkler.com>
2021-04-03 15:22:17 +0530

    chore: re-orgainized folders

--- .storybook/react-mark.js/main.js
+++ .storybook/react-mark.js/main.js
@@ -2,7 +2,7 @@ module.exports = {
   stories: [
     // "../stories/**/*.stories.mdx",
     // "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-    "../../packages/react-mark.js/src/**/*.stories.js",
+    "../../multi-packages/react-mark.js/main/src/**/*.stories.js",
   ],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
 };
