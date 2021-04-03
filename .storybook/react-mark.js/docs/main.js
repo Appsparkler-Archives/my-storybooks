4565dfd6ed24dc019791d50050845f45fc59be72
Aakash Shah <subs@appsparkler.com>
2021-04-03 17:06:15 +0530

    chore: setting up docs for react-mark

---
+++ .storybook/react-mark.js/docs/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  stories: [
+    "../../../multi-packages/react-mark.js/docs/src/**/*.stories.mdx",
+    "../../../multi-packages/react-mark.js/docs/**/*.stories.mdx",
+    // "../../../multi-packages/react-mark.js/docs/",
+    // "../stories/**/*.stories.mdx",
+    // "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    // "../../multi-packages/react-mark.js/main/src/**/*.stories.js",
+  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+};
