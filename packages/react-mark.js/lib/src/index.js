cdfc1858a5439e474f80c4e7cc2240dedb699ac2
Aakash Shah <subs@appsparkler.com>
2021-04-11 20:21:08 +0530

    fix: testing build pipeline

--- packages/react-mark.js/lib/src/index.js
+++ packages/react-mark.js/lib/src/index.js
@@ -1,7 +1,10 @@
 import RegExpMarker from "./RegExpMarker";
 import Marker from "./Marker";
 import RangesMarker from "./RangesMarker";
-// import useMarker from "./useMarker";
 import setupMarker from "./setupMarker";
 
 export { RegExpMarker, Marker, RangesMarker, setupMarker as useMarker };
+
+(() => {
+  // the test run
+})();
