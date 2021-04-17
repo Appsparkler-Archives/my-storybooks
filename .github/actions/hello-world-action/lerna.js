842529f5757ca5ffc8a62b443534f0b0355767b8
Aakash Shah <subs@appsparkler.com>
2021-04-17 11:36:10 +0530

    ci: removed un-utilized files

--- .github/actions/hello-world-action/lerna.js
+++
@@ -1,5 +0,0 @@
-const { execSync } = require("child_process");
-const fs = require("fs");
-fs.writeFileSync("./changed.json", "");
-console.log(Object.keys(fs));
-execSync("lerna changed --json | ./changed.json");
