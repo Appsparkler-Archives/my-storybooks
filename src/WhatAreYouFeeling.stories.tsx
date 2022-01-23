d8877a74775be87b4e6b9bf18885dc854a6134cf
Aakash Shah <subs@appsparkler.com>
2022-01-23 15:32:27 +0530

    chore: add start script, setup what-are-you-feeling component

--- src/WhatAreYouFeeling.stories.tsx
+++ src/WhatAreYouFeeling.stories.tsx
@@ -1,14 +1,33 @@
-import { ComponentMeta, Meta, ComponentStory } from "@storybook/react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
 import { WhatAreYouFeelingProps, WhatAreYouFeeling } from "./WhatAreYouFeeling";
+import { action } from "@storybook/addon-actions";
+import { useCallback, useState } from "react";
 
 export default {
   title: "NVC App",
   component: WhatAreYouFeeling,
 } as ComponentMeta<typeof WhatAreYouFeeling>;
 
-const Template: ComponentStory<typeof WhatAreYouFeeling> = (args) => (
-  <WhatAreYouFeeling {...args} />
-);
+const Template: ComponentStory<typeof WhatAreYouFeeling> = ({
+  value: $value,
+  onChange,
+  feelings,
+}) => {
+  const [value, setValue] = useState($value);
+  const handleChange = useCallback((value) => {
+    // action('onChange')(value)
+    onChange(value);
+    setValue(value);
+  }, []);
+
+  return (
+    <WhatAreYouFeeling
+      feelings={feelings}
+      value={value}
+      onChange={handleChange}
+    />
+  );
+};
 Template.args = {} as WhatAreYouFeelingProps;
 Template.args = {};
 
